f55767a2594b4c21daa681f897bc059b1bebbcbc
Julian <86843980+Juliandreslopez@users.noreply.github.com>
2025-06-16 14:51:10 -0400

    Add files via upload

---
+++ secrets/ec2.tf
@@ -0,0 +1,6 @@
+# Snippet from  main.tf
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIAQXMOCKZGRHNO5L4N"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
---
+++ secrets/main.tf
@@ -0,0 +1,6 @@
+# Snippet from  main.tf
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAQXMOCKZGRHNO5L4N"
+  secret_key = "nahuXNnSCNyexAMpleJI/NNyEoah684Qu7GyFyux"
+}
